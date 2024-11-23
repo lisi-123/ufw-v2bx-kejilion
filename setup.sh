c8ff1c4e0841ebd515ca36065f1ccc1d35b1027c
Travelers2024 <160303845+Travelers2024@users.noreply.github.com>
2024-11-23 12:44:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ sudo iptables -t nat -A PREROUTING -p udp --dport 35500:35600 -j REDIRECT --to-p
 sudo ufw --force enable
 
 # 执行其他安装指令
-wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh && bash install.sh
+wget -N https://raw.githubusercontent.com/wyx2685/V2bX-script/master/install.sh && bash install.sh v0.0.18
 curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
 
 # 添加定时任务
