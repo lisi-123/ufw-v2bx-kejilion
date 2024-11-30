38f67c1ab98d91dc04a12f3da3d3e85e725362ec
Travelers2024 <160303845+Travelers2024@users.noreply.github.com>
2024-11-30 15:03:32 +0800

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -3,5 +3,7 @@
 # 先下载并执行 menu.sh 脚本
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
 
-# 替换配置文件
-wget -N https://github.com/<用户>/<仓库>/raw/main/path/to/file -P /path/to/target/directory
+# 替换配置文件 sing_origin.json
+wget -N https://raw.githubusercontent.com/Travelers2024/ufw-v2bx-kejilion/main/sing_origin.json -P /etc/V2bX/
+
+echo "已自动配置warp解锁"
