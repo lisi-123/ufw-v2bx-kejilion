cbadc8ed446c1664fe42775267753018c6178524
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2024-12-05 00:07:37 +0800

    Update setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -4,6 +4,6 @@
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
 
 # 替换配置文件 sing_origin.json
-wget -N https://raw.githubusercontent.com/Travelers2024/ufw-v2bx-kejilion/main/sing_origin.json -P /etc/V2bX/
+wget -N https://raw.githubusercontent.com/lisi-123/ufw-v2bx-kejilion/main/sing_origin.json -P /etc/V2bX/
 
 echo "已自动配置warp解锁，重启v2bx生效"
